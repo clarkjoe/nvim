8281cfccd77468736f2cd2f5014da8fe31aff975
Joe Clark <clarkjoe.co@gmail.com>
2023-11-10 09:57:53 -0700

    update telescope

--- lua/clarkjoe/packer.lua
+++ lua/clarkjoe/packer.lua
@@ -45,7 +45,7 @@ return require('packer').startup(function(use)
         }
         -- Telescope (fuzzy finder)
         use {
-            'nvim-telescope/telescope.nvim', tag = '0.1.0',
+            'nvim-telescope/telescope.nvim', tag = '0.1.4',
             -- or                            , branch = '1.1.x',
             requires = { { 'nvim-lua/plenary.nvim' } }
         }
