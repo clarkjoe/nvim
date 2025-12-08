ece46ee1278fb130ddfc1fad6fd86e41a684dbf5
Joe Leland Clark <clarkjoe.co@gmail.com>
2025-12-08 10:46:30 -0700

    add gruvbox

--- lua/plugins/theme.lua
+++ lua/plugins/theme.lua
@@ -1,5 +1,12 @@
 return {
-  'sainnhe/gruvbox-material',
+  'morhetz/gruvbox',
   lazy = false,
   priority = 1000,
+  config = function()
+    -- Your gruvbox configuration here
+    vim.g.gruvbox_contrast_dark = 'medium'
+    vim.g.gruvbox_italic = 1
+    vim.cmd([[colorscheme gruvbox]])
+    vim.opt.background = 'dark'
+  end,
 }
