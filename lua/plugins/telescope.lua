2c8022bbd33bf7ffb235d65cec34e64249a26e82
Joe Leland Clark <clarkjoe.co@gmail.com>
2026-01-06 18:13:59 +0000

    update telescope config

--- lua/plugins/telescope.lua
+++ lua/plugins/telescope.lua
@@ -1,6 +1,7 @@
 return {
   "nvim-telescope/telescope.nvim",
   tag = "0.1.8",
+  lazy = false,
   dependencies = { "nvim-lua/plenary.nvim" },
   config = function()
     require("telescope").setup({
@@ -29,13 +30,18 @@ return {
 
     keymap.set("n", "<leader>pf", builtin.find_files, { desc = "Find files" })
     keymap.set("n", "<leader>pb", builtin.buffers, { desc = "Find buffers" })
-    keymap.set("n", "<C-p>", builtin.git_files, { desc = "Find git files" })
-    
+    keymap.set("n", "<C-p>", function()
+        local ok = pcall(builtin.git_files)
+        if not ok then
+            builtin.find_files()
+        end
+    end, { desc = "Find git files (or all files)" })
+
     -- Requires ripgrep (https://github.com/BurntSushi/ripgrep)
     keymap.set("n", "<leader>ps", function()
       builtin.grep_string({ search = vim.fn.input("Grep > ") })
     end, { desc = "Grep string" })
-    
+
     keymap.set("n", "<leader>a", builtin.lsp_document_symbols, { desc = "LSP document symbols" })
   end,
 }
